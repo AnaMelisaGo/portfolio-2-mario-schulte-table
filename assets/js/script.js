e9c041ce2f8c656c879e7b47d7650a0838226b72
AnaMelisaGo <melisago.rockn@gmail.com>
2021-12-28 06:23:21 +0000

    Style the header

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* game menu */
+/* let barButton = document.getElementById('game-icon');
+barButton.addEventListener('click', myFunction); */
+
+function toggleGameMenu(menu) {
+    menu.classList.toggle('open')
+}
