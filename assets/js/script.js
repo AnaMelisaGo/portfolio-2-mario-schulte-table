815ea9406d38d2445edf9f1e7c45443eb77cd0a5
AnaMelisaGo <melisago.rockn@gmail.com>
2021-12-29 17:34:05 +0000

    Add the table and the styles, and the add some script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-/*Game menu toggle*/
+/* ---------------------------------------------- Navigation bar --------------------------- */
+/**
+ * A function to toggle between class
+ * for responsive navigation bar
+ * 
+ */
 function toggleGameMenu(menu) {
     this.classList.toggle('open')
 };
@@ -6,3 +11,15 @@ function toggleGameMenu(menu) {
 let menuButton = document.getElementById('icon-game');
 menuButton.addEventListener('click', toggleGameMenu)
 
+/* ---------------------------------------------- The Schulte Table --------------------------- */
+let playerData = {
+    name: 'Papa'
+}
+
+let playerName = document.getElementsByClassName('player-name');
+for(player of playerName) {
+    
+    console.log(player.innerHTML);
+    console.log(playerData.name);
+    player.innerHTML = playerData.name;
+}
